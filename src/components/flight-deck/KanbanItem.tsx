4165f64f4830ad4cbb5e599a7374888577296751
jhonangelob <bustarde.aj@gmail.com>
2026-05-27 17:45:30 +0800

    chore/responsive-ui

--- src/components/flight-deck/KanbanItem.tsx
+++ src/components/flight-deck/KanbanItem.tsx
@@ -2,8 +2,6 @@ import { formatNumberCompact } from '#/helper/number'
 
 import { useNavigate } from '@tanstack/react-router'
 
-import { cn } from '#/lib/utils'
-
 import type { Application } from '#/validators/application'
 
 export interface KanbanItemProps {
@@ -30,8 +28,8 @@ export default function KanbanItem({ data }: KanbanItemProps) {
       className="hover:border-primary-text flex cursor-pointer flex-col rounded-md border bg-white px-3.5 pt-3.5 pb-3"
       onClick={handleClickApplicationItem}
     >
-      <div className="text-muted flex flex-row justify-between gap-2 pb-1 font-mono text-[11px] leading-[1.4] tracking-[0.4px]">
-        <p>{data.company}</p>
+      <div className="text-muted flex flex-row justify-between gap-3 pb-1 font-mono text-[11px] leading-[1.4] tracking-[0.4px]">
+        <p className="truncate">{data.company}</p>
         <p className="text-nowrap">
           {data.appliedAt &&
             new Date(data.appliedAt).toLocaleDateString('en-US', {
@@ -44,25 +42,12 @@ export default function KanbanItem({ data }: KanbanItemProps) {
         {data.role}
       </p>
 
-      <p
-        className={cn(
-          'text-primary-text items-center font-mono text-[13px] leading-[1.4] font-medium',
-          data.salaryRange || (data.status && 'border-t pt-2'),
-        )}
-      >
-        {data.salaryRange && (
-          <span>
-            P
-            {data.salaryRange &&
-              formatNumberCompact(Number(data.salaryRange))}{' '}
-          </span>
-        )}
+      <p className="text-primary-text items-center border-t pt-2 font-mono text-[13px] leading-[1.4] font-medium">
+        <span>P{formatNumberCompact(Number(data.salaryRange))} - </span>
 
-        {data.status && (
-          <span className="text-muted font-sans text-[12px] leading-[1.4] font-normal">
-            {data.status}
-          </span>
-        )}
+        <span className="text-muted font-sans text-[12px] leading-[1.4] font-normal">
+          {data.status || ''}
+        </span>
       </p>
     </div>
   )
