f3f77b863ed3d5041ee2a2a3ffe5113620d31397
jhonangelob <bustarde.aj@gmail.com>
2026-06-02 07:22:07 +0800

    chore/text-update

--- src/components/co-pilot/ApplicationSummary.tsx
+++ src/components/co-pilot/ApplicationSummary.tsx
@@ -24,7 +24,7 @@ export default function ApplicationSummary({ data }: ApplicationSummaryProps) {
     },
 
     {
-      label: 'Stages to landed',
+      label: 'Stages before landing',
       value: stagesToLand,
     },
     {
