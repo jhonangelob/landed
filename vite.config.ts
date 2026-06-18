ae770eecf32f1cf91b55afa530da1f454ab1aab8
jhonangelob <bustarde.aj@gmail.com>
2026-06-18 21:27:22 +0800

    fix/initial-deployment-issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,21 @@ import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
+import { nitro } from 'nitro/vite'
+
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), svgr()],
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    nitro(),
+    viteReact(),
+    svgr(),
+  ],
 })
 
 export default config
